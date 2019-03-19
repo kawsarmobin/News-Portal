23e8d60bf8e1f3204c707eabbcf1d14c3f204298
kawsar <hmkawsar559@gmail.com>
2019-03-19 15:11:00 +0600

    Admin templete setup completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,30 @@
-@extends('layouts.app')
-
+@extends('layouts.master') 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+<!-- Blockquotes -->
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    BLOCKQUOTES
+                </h2>
+            </div>
+            <div class="body">
+                <blockquote class="m-b-25">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                </blockquote>
+                <blockquote>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                </blockquote>
+                <blockquote class="blockquote-reverse">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                </blockquote>
             </div>
         </div>
     </div>
 </div>
+<!-- #END# Blockquotes -->
 @endsection
