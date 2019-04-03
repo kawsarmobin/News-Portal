b9a9b0979853c074faab2cd8922fd65ddf76fb87
kawsar <hmkawsar559@gmail.com>
2019-04-03 14:30:48 +0600

    fav icon meta tag added

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         @yield('meta')
         <meta name="author" content="">
+        <meta property="og:image" content="{{ asset('img/logo.png') }}">
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
